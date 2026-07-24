88383d0e72d57eead19b8042baf0e0dd5e481d3e
abhinna369 <sundruuu25@gmail.com>
2026-07-24 23:45:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const heading = document.querySelector("h1");
+const message = document.querySelector("p");
+
+setTimeout(() => {
+heading.textContent = "Happy 9 Beautiful Months ❤️";
+}, 2000);
+
+setTimeout(() => {
+message.textContent = "Meriii Chuiimuii 🌻\nI'm with uuu in every smile, every tear, every dream, nd every heartbeat. Everything is gonna be alright. I love uuu endlessly. ❤️";
+}, 4000);
+
+});
